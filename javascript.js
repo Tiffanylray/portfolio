ce67cd6651c13a0fa8a184f8ae98ce19d79890c2
Tiffany Ray <tiffanyraylee@gmail.com>
2019-01-08 14:52:26 -0600

    highlighing nav links as the page scrolls through sections

--- javascript.js
+++ javascript.js
@@ -2,14 +2,25 @@ $(document).ready(() => {
   let deviceWidth = $(window).width();
   let deviceHeight = $(window).height();
 
-  //Sticky Scroll Nav Bar
+  // Button Scroll Function
+  $('a.scrollLink').on('click', function(event) {
+    event.preventDefault();
+    $('html, body').animate(
+      {scrollTop: $($(this).attr('href')).offset().top }, 500);
+  });
+
+
+  //Nav Bar
   $(window).on('scroll', function() {
     let pos = $(window).scrollTop();
+    let sectionTop = pos + 70;
     let navBar = $('#nav-bar');
-    let nav = $('nav');
+    let nav = navBar.find('nav');
+    let navUl = nav.find('.nav-ul');
     let navPos = navBar.position().top;
-    let lastPos = 0;
 
+
+    // Sticky Nar Bar
     if (pos >= deviceHeight) {
       navBar.addClass('display');
       nav.addClass('fixed');
@@ -18,10 +29,25 @@ $(document).ready(() => {
       nav.removeClass('fixed');
       navBar.removeClass('display');
     };
+
+    //Highlighing Nav Links
+
+    if (sectionTop > $('#skills').offset().top) {
+      highlightLink('skills-a');
+    }
+    if (sectionTop > $('#projects').offset().top) {
+      highlightLink('projects-a');
+    }
+    if (sectionTop > $('#about').offset().top) {
+      highlightLink('about-a');
+    };
+
+    function highlightLink(id) {
+      $('.nav-ul .active').removeClass('active');
+      nav.find('[id="' + id + '"]').addClass('active');
+    };
+
   });
-
-
-
 
 
 
